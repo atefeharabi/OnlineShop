17eb5dabafd0941d4090b0e5c2210235bbe62383
atefeharabi <atefeh.araabi@gmail.com>
2023-02-03 00:06:23 +0330

    030: prepopulated_fields added and admin.site.register changed for sync slug with name

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category, Discount, Feature
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
 admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Discount)
 admin.site.register(Feature)
