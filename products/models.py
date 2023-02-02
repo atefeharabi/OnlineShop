911589fb6730a0d7f87de702fef2e8879b24eba2
atefeharabi <atefeh.araabi@gmail.com>
2023-02-02 21:35:22 +0330

    009: Product model added to products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, db_index=True)
+    manufacturer = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=500, db_index=True)
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    expired = models.DateField(null=True, blank=True)
+    discount = models.ForeignKey('Discount', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
