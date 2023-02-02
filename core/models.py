f0a2b3d12476c42d6f19f724c4b9478f06494f8f
atefeharabi <atefeh.araabi@gmail.com>
2023-02-02 21:22:35 +0330

    007: Country, State, City classes added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class State(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
