e375970ada2d5b20bf4f5fd7522ffa13cc0488e0
atefeharabi <atefeh.araabi@gmail.com>
2023-02-02 22:40:20 +0330

    022: Country, State, City model added to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Country, State, City
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('f_name', )}
+
+
+class StateAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('f_name', )}
+
+
+class CityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('f_name', )}
+
+
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
