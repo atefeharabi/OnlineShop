2b2e975cfb7302c9bbaa392d882bccb9b69df40f
atefeharabi <atefeh.araabi@gmail.com>
2023-02-02 23:49:24 +0330

    029: Customer model import corrected

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-# from .managers import CustomerManager
+from .managers import CustomerManager
 from core.models import Country, State, City
 
 
