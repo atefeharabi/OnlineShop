a346beb78ab2ae9eb7106795fb14362d2d6ffcdd
atefeharabi <atefeh.araabi@gmail.com>
2023-02-02 23:18:45 +0330

    026: prepopulated_fields was corrected

--- core/admin.py
+++ core/admin.py
@@ -3,15 +3,15 @@ from .models import Country, State, City
 
 
 class CountryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('f_name', )}
+    prepopulated_fields = {'slug': ('name', )}
 
 
 class StateAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('f_name', )}
+    prepopulated_fields = {'slug': ('name', )}
 
 
 class CityAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('f_name', )}
+    prepopulated_fields = {'slug': ('name', )}
 
 
 admin.site.register(Country)
