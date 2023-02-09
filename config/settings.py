a0471a5ba047ae10019e9c112daf2040bbe76573
atefeharabi <atefeh.araabi@gmail.com>
2023-02-09 16:51:29 +0330

    038: (hotfix: 004) STATIC_DIRS was set.

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_DIRS = [BASE_DIR/'static']
 MEDIA_URL = '/datastorage/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
