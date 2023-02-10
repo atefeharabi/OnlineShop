8e687d7f24aef79a4e064383408661aab89546dd
atefeharabi <atefeh.araabi@gmail.com>
2023-02-10 08:17:33 +0330

    041: set templates folders with first view

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_DIRS = [BASE_DIR/'static']
-MEDIA_URL = '/datastorage/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
