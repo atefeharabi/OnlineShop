5d69bdb304eed477b8e0fed8324a6703c3f49332
atefeharabi <atefeh.araabi@gmail.com>
2023-02-17 01:14:55 +0330

    064: (hotfix: 018) customize Order and OrderItem models in admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'order_date', 'payment_status', 'order_status', 'total_amount')
+    ordering = ('order_date', 'customer')
+    list_filter = ('order_status', 'payment_status')
+    search_fields = ('customer',)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'number_of_order', 'order')
+    ordering = ('product', )
+    list_filter = ('order', )
+    search_fields = ('product',)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
