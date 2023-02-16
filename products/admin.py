9ce5a700e2dc8bc6623b2f785147fe758f7ed017
atefeharabi <atefeh.araabi@gmail.com>
2023-02-17 00:35:21 +0330

    060: (hotfix: 014) customize Category model in admin panel

--- products/admin.py
+++ products/admin.py
@@ -4,10 +4,15 @@ from .models import Product, Category, Discount, Feature
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
+    list_display = ('name', 'sub_category', 'is_sub')
+    ordering = ('name', 'sub_category')
+    list_filter = ('is_sub',)
+    search_fields = ('name', 'sub_category')
 
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('manufacturer', 'name')}
+
 
 
 admin.site.register(Product, ProductAdmin)
