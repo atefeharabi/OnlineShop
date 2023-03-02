60b2db8367173ef8ac127238694b182a62e4c307
atefeharabi <atefeh.araabi@gmail.com>
2023-03-03 02:09:52 +0330

    074: Order and OrderItem models was changed

--- orders/admin.py
+++ orders/admin.py
@@ -2,19 +2,26 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'order_date', 'payment_status', 'order_status', 'total_amount')
+    list_display = ('customer', 'order_date', 'payment_status', 'order_status', 'updated')
     ordering = ('order_date', 'customer')
     list_filter = ('order_status', 'payment_status')
     search_fields = ('customer',)
+    inlines = (OrderItemInline,)
 
 
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('product', 'number_of_order', 'order')
-    ordering = ('product', )
-    list_filter = ('order', )
-    search_fields = ('product',)
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display = ('product', 'order', 'price', 'quantity')
+#     ordering = ('product', )
+#     list_filter = ('order', )
+#     search_fields = ('product',)
 
 
-admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderItem, OrderItemAdmin)
+# admin.site.register(Order, OrderAdmin)
+# admin.site.register(OrderItem, OrderItemAdmin)
--- orders/models.py
+++ orders/models.py
@@ -7,18 +7,39 @@ class Order(models.Model):
     PAYMENT = [('PA', 'Paid'), ('UP', 'Unpaid')]
     ORDER_STATUS = [('C', 'Confirmed'), ('D', 'Delivered'), ('P', 'Processed')]
     order_date = models.DateField(auto_now_add=True)
-    amount = models.FloatField()
-    payment_status = models.CharField(choices=PAYMENT, max_length=2)
+    # price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    payment_status = models.CharField(choices=PAYMENT, max_length=2, default=PAYMENT[1])
     order_status = models.CharField(choices=ORDER_STATUS, max_length=1)
     use_discount = models.BooleanField()
-    total_amount = models.FloatField()
+    # price_after_discount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ('payment_status', 'updated')
+
+    def __str__(self):
+        return f"{self.customer} - {self.id}"
+
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def after_discount(self):
+        pass
 
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    number_of_order = models.IntegerField()
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.id
+
+    def get_cost(self):
+        return self.price * self.quantity
+
 
 
 
